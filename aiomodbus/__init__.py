38a83c338655326f18979d63721736c2b3754367
pazzarpj <ryan.parry-jones@ampcontrolgroup.com>
2019-12-02 19:55:46 +1100

    Reworked the watchdog code

--- aiomodbus/__init__.py
+++ aiomodbus/__init__.py
@@ -70,6 +70,32 @@ modbus_exception_codes = {
 }
 
 
+class Watchdog:
+    def __init__(self, check_interval: float):
+        self.check_interval = check_interval
+        self.cancel_flag = False
+        self.started = True
+        self.future = None
+
+    def feed(self):
+        self.cancel_flag = False
+        if not self.started:
+            self.start()
+
+    def start(self):
+        asyncio.create_task(self._loop(self.future))
+
+    async def _loop(self, future: asyncio.Future):
+        while True:
+            await asyncio.sleep(self.check_interval)
+            if future.done():
+                break
+            if self.cancel_flag:
+                future.cancel()
+            self.cancel_flag = True
+        self.started = False
+
+
 class RequestSerial:
     function_code = 0
     exception_code = 0
@@ -78,12 +104,13 @@ class RequestSerial:
         self.lock = lock
         self.transport = transport
         self.protocol = protocol
-        self.timeout_flag = asyncio.Event()
         self.future = None
         self.data = bytearray()
         self.response_length = 0
         self.exception_length = 0
         self.timer = None
+        self.request_packing = ""
+        self.decode_packing = ""
         if transport.serial.baudrate > 19200:
             self.t_1_5 = 750e-6
             self.t_3_5 = 1.75e-3
@@ -101,45 +128,33 @@ class RequestSerial:
         # second intervals. That means we can't enforce the inter byte timeout as set in the modbus standard
         if self.t_1_5 < 0.1:
             self.t_1_5 = 0.1
-        if self.t_3_5 < 0.1:
-            self.t_3_5 = 0.1
-
-    def watchdog_feed(self):
-        if self.timer is not None:
-            self.timer.cancel()
-        self.timer = asyncio.ensure_future(self.timeout_task())
-
-    async def timeout_task(self):
-        """
-        Purpose is to trigger on an inter byte timeout since the pyserial library isn't doing it correctly.
-        Currently the python processing and receiving is interferring with the tight timing controls. Need to revisit
-        this function
-        :return:
-        """
-        await asyncio.sleep(self.t_1_5)
-        self.timeout_flag.set()
-        await asyncio.sleep(self.t_1_5)
-        if not self.future.done():
-            self.future.cancel()
-
-    async def __aenter__(self):
-        await self.lock.acquire()
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await asyncio.sleep(self.t_3_5)
-        await self.lock.release()
+        # if self.t_3_5 < 0.1:
+        # self.t_3_5 = 0.1
+        self.wachdog = Watchdog(self.t_3_5)
 
     def data_recv(self, data):
-        self.watchdog_feed()
+        self.wachdog.feed()
         self.data.extend(data)
         # TODO check exception code responses
         if len(self.data) == self.exception_length and self.data[1] == self.exception_code:
             self.protocol.recv_callback = None
             self.future.set_exception(modbus_exception_codes.get(self.data[2], IOError))
-        elif len(self.data) == self.response_length:
+        elif len(self.data) >= self.response_length:
             self.protocol.recv_callback = None
             self.future.set_result(self.data)
+
+    def request_packet(self, address, count, unit) -> bytearray:
+        packet = bytearray()
+        packet.extend(struct.pack(self.request_packing, unit, self.function_code, address, count))
+        crc = aiomodbus.crc.calc_crc(packet)
+        packet.extend(struct.pack(">H", crc))
+        return packet
+
+    def decode(self, packet: bytearray, unit):
+        unit_id, code, cnt, *values, crc = struct.unpack(self.decode_packing, packet)
+        assert unit_id == unit
+        assert code == self.function_code
+        return values
 
 
 class ReadHoldingRegistersRTU(RequestSerial):
@@ -149,33 +164,54 @@ class ReadHoldingRegistersRTU(RequestSerial):
 
     async def transaction(self, address, count, unit, timeout=None):
         async with self.lock:
-            self.timeout_flag.clear()
             self.data.clear()
+            self.request_packing = ">BBHH"
+            self.decode_packing = ">BBBH" + "H" * count
             try:
                 packet = self.request_packet(address, count, unit)
                 self.response_length = 5 + count * 2
                 self.future = asyncio.Future()
                 self.protocol.set_recv_callback(self.data_recv)
+                print(f"writing: packet")
                 self.transport.write(packet)
+                self.wachdog.future = self.future
                 # TODO replace timeout with turn around time and change to first completed wait
-                response = await asyncio.wait_for(self.future, timeout)
+                response = await asyncio.wait_for(self.future, timeout=0.3)
+                # print("Check CRC")
                 aiomodbus.crc.check_crc(response)
-                return self.decode(response, count, unit)
+                return self.decode(response, unit)
+            except:
+                await asyncio.sleep(0.01)
+                raise
             finally:
                 await asyncio.sleep(self.t_3_5)
 
-    def request_packet(self, address, count, unit) -> bytearray:
-        packet = bytearray()
-        packet.extend(struct.pack(">BBHH", unit, self.function_code, address, count))
-        crc = aiomodbus.crc.calc_crc(packet)
-        packet.extend(struct.pack(">H", crc))
-        return packet
 
-    def decode(self, packet: bytearray, count, unit):
-        unit_id, code, cnt, *values, crc = struct.unpack(">BBBH" + "H" * count, packet)
-        assert unit_id == unit
-        assert code == self.function_code
-        return values
+class WriteSingleRegisterRTU(RequestSerial):
+    function_code = 0x06
+    exception_code = 0x86
+    exception_length = 5
+
+    async def transaction(self, address, value, unit, timeout=None):
+        async with self.lock:
+            self.data.clear()
+            self.request_packing = ">BBHH"
+            self.decode_packing = ">BBHHH"
+            try:
+                packet = self.request_packet(address, value, unit)
+                self.response_length = 8
+                self.future = asyncio.Future()
+                self.protocol.set_recv_callback(self.data_recv)
+                self.transport.write(packet)
+                response = await asyncio.wait_for(self.future, timeout=0.3)
+                # TODO replace timeout with turn around time and change to first completed wait
+                aiomodbus.crc.check_crc(response)
+                return self.decode(response, unit)
+            except:
+                await asyncio.sleep(0.01)
+                raise
+            finally:
+                await asyncio.sleep(self.t_3_5)
 
 
 class ModbusSerialProtocol(asyncio.Protocol):
@@ -183,6 +219,7 @@ class ModbusSerialProtocol(asyncio.Protocol):
         self.transport = None
         self.connected = asyncio.Event()
         self.current_request = None
+        self.recv_callback = None
 
     def connection_made(self, transport):
         self.transport = transport
@@ -193,6 +230,8 @@ class ModbusSerialProtocol(asyncio.Protocol):
     def data_received(self, data):
         if self.recv_callback:
             self.recv_callback(data)
+        else:
+            print(f"Uncaught data: {data}")
 
     def set_recv_callback(self, handle):
         self.recv_callback = handle
@@ -209,40 +248,6 @@ class ModbusSerialProtocol(asyncio.Protocol):
     def resume_writing(self):
         print(self.transport.get_write_buffer_size())
         print('resume writing')
-
-    async def transaction(self, request: RequestSerial):
-        async with request:
-            self.current_request = request
-
-    def read_request(self, unit, function_code, *values):
-        packet = bytearray()
-        packet.extend(struct.pack(">BB" + "H" * len(values), unit, function_code, *values))
-        crc = aiomodbus.crc.calc_crc(packet)
-        packet.extend(struct.pack(">H", crc))
-        self.transport.write(packet)
-        self.future = asyncio.Future()
-        return self.future
-
-    def write_multiple_request(self, unit, function_code, address, *values, reg_size=16):
-        packet = bytearray()
-        if reg_size == 8:
-            typ = "B"
-            byt_cnt = len(values)
-        else:
-            typ = "H"
-            byt_cnt = len(values) * 2
-        packet.extend(struct.pack(">BBHB" + typ * len(values), unit, function_code, address, byt_cnt, *values))
-        crc = 0x95cb
-        assert crc == aiomodbus.crc.calc_crc(packet)
-        packet.extend(struct.pack(">H", crc))
-        self.transport.write(packet)
-
-    def read_holding_registers(self, address, count, unit=1):
-        packet = bytearray()
-        function_code = 0x03
-        crc = 0x95cb
-        packet.extend(struct.pack(">BBHHH", unit, function_code, address, count, crc))
-        self.transport.write(packet)
 
 
 @dataclass
@@ -278,6 +283,7 @@ class ModbusSerial:
             self.t_3_5 = t_0 * 3.5
         await self.protocol.connected.wait()
         self._read_holding_registers = ReadHoldingRegistersRTU(self.transaction, self.transport, self.protocol)
+        self._write_holding_register = WriteSingleRegisterRTU(self.transaction, self.transport, self.protocol)
 
     async def read_coils(self, address, count, *, unit=None, timeout=None):
         async with self.transaction:
@@ -315,12 +321,7 @@ class ModbusSerial:
                                           timeout=timeout)
 
     async def write_single_register(self, address, value, *, unit=None, timeout=None):
-        async with self.transaction:
-            if unit is None:
-                unit = self.default_unit_id
-            function_code = 0x06
-            return await asyncio.wait_for(self.protocol.read_request(unit, function_code, address, value),
-                                          timeout=timeout)
+        return await self._write_holding_register.transaction(address, value, unit or self.default_unit_id, timeout)
 
     async def write_multiple_coils(self, address, *values, unit=None, timeout=None):
         async with self.transaction:
@@ -355,6 +356,9 @@ class ModbusSerial:
 
 
 if __name__ == "__main__":
+    import random
+
+
     async def main():
         client = ModbusSerial("COM14", baudrate=9600, parity="E", stopbits=2)
         await client.connect()
@@ -364,7 +368,11 @@ if __name__ == "__main__":
         #     baudrate=9600, parity="E", stopbits=2)
         while True:
             try:
-                print(await client.read_holding_registers(1, 2, unit=1))
+                print(await client.read_holding_registers(1, 200, unit=1))
+            except BaseException as e:
+                log.exception(e)
+            try:
+                print(await client.write_single_register(random.choice([1, 2]), random.randint(1, 100), unit=1))
             except BaseException as e:
                 log.exception(e)
             # transport.write(b"HI THERE")
